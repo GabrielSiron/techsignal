3cee89c2a274a9c1d048427afdaec1c5c3ba2191
Gabriel Menezes <=>
2020-10-27 23:44:03 -0300

    add module.ts files

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -4,6 +4,7 @@ import { NavController, NavParams } from 'ionic-angular';
 import { Cadastro} from '../cadastro/cadastro';
 import { from } from 'rxjs/observable/from';
 import { directive } from '@angular/core/src/render3/instructions';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'other-page',
@@ -17,18 +18,18 @@ export class Login {
   estadobool: boolean;
   src: string;
   aux: string;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public formBuilder: FormBuilder, public navCtrl: NavController, public navParams: NavParams) {
     this.estado = "password";
     this.estadobool = true;
     this.aux = 'a';
     this.src = "../assets/imgs/novalogo.png";
+
   }
       logIn(login, senha) {
         this.aux = login;
         console.log(login);
-          if(login == 'password'){
-            this.navCtrl.push(HomePage);
-          }
+        this.navCtrl.push(HomePage);
+
     };
 
       cad(){
